b7fcefa476bb217907498c3d68ce5336037cdb52
Chinmaya <technical@treezenterprise.com>
2026-04-27 21:56:52 +0530

    rename instance profile

--- terraform/iam-role.tf
+++ terraform/iam-role.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
 
 # Instance Profile
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-profile"
+  name = "ec2-cloudwatch-profile"
   role = aws_iam_role.ec2_cloudwatch_role.name
 }
 
